f150f348503ae4c26cf89b3c500890b593005cc3
hamzamain <mohammadmainuddin211@gmail.com>
2022-09-22 16:03:21 +0600

    first commit

---
+++ js/error.js
@@ -0,0 +1,19 @@
+// 1.SyntaxError----------------------------------->
+
+/* console.log(5);
+function add() {
+  console.log("hello");
+} */
+
+// Let x = 10;
+
+// cosnt y = 7;
+
+// 2.TypeError-------------------------------------->
+
+let a;
+console.log(typeof a);
+// console.log(a.length);
+
+const people = 5;
+people();
---
+++ js/practice.js
@@ -0,0 +1,41 @@
+/* setTimeout(() => {
+  console.log("hello");
+}, 3500); */
+
+/* const showNameAge = () => {
+  const locat = location.toString();
+  alert(`your location address is ${locat}`);
+};
+
+const showConfirm = () => {
+  const isYes = confirm("will you show your location");
+  if (isYes === true) {
+    console.log(isYes);
+    showNameAge();
+  }
+};
+
+function promptAlert() {
+  const name = prompt("what is your name", "kamrul");
+  const result = name + 200;
+  showConfirm();
+  //   showNameAge(result);
+}
+promptAlert();
+ */
+
+/* 
+Cookies:
+1.cookies are data stored in small text files, on your/users computer.
+2.it's solve the provlem to remember user information like username
+3.when a web server has sent a web page to browser.the connection is shut down, and server forget everything about user. that's why cookies are used.
+4.cookies are saved in name-value pair like --> Username = Jhon Due.
+5.cookies can be make by javascipt ---> document.cookies ="username= Jhon Due";
+*/
+
+/* 
+sesion storage vs local storage 
+1.accecibility: only one tab or window vs all window and tab
+2.expire: untill tab close vs untill manually delete
+3.data temporary vs permanent data
+*/
